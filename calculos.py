8db015ae0b7421e2ada5affae1d44a680f0487ef
Balta <71284219+BaltaCD@users.noreply.github.com>
2020-10-02 20:36:42 -0600

    Add files via upload

---
+++ calculos.py
@@ -0,0 +1,112 @@
+GREEN = '\033[32m'
+RED = '\033[31m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+WHITE = '\033[37m'
+
+print(MAGENTA+"Author:")
+print("╔╗","","","","","","","","","","","╔╗")
+print("║╚╗","╔═╗","","╔╗","","║╚╗","╔═╗")
+print("║╬║","║╬╚╗","║╚╗","║╔╣","║╬╚╗")
+print("╚═╝","╚══╝","╚═╝","╚═╝","╚══╝")
+
+print("")
+
+print(GREEN+"Hola mucho gusto")
+print(GREEN+"soy un programa que realiza sumas, restas,")
+print(GREEN+"multiplicaciones, divisiones,")
+print(GREEN+"entregó el residuo de una división")
+print(GREEN+"y elevo un número a la potencia.")
+
+print("")
+
+nombre = input(RED+">> ¿desea utilizarme?: ")
+print(GREEN+"¿",nombre, "?,","muy bien")
+
+if nombre == "no":
+    print("Entonces vete.")
+if nombre == "No":
+    print("Entonces vete.")
+if nombre == "NO":
+    print("Entonces vete.")
+if nombre == "nop":
+    print("Entonces vete.")
+if nombre == "Nop":
+    print("Entonces vete.")
+if nombre == "NOP":
+    print("Entonces vete.")
+
+print("")
+
+print(BLUE+"..:acontinuación digite los números a utilizar:..")
+
+numero1 = float(input(RED+">> Digite un número: "))
+numero2 = float(input(RED+">> Digite un segundo número: "))
+
+print(MAGENTA+"")
+
+print("","╔═╗")
+print("╔╝ ╚╗","","╔═══╗")
+print("╚╗ ╔╝","","╚═══╝")
+print("","╚═╝")
+
+print(RED+"")
+
+print("a)suma")
+print("b)resta")
+print("c)multiplicacion")
+print("d)división")
+print("e)residuo de división")
+print("f)elevar a la potencia")
+e = input(RED+">> ¿Qué operación desea realizar? " )
+print("")
+
+if e == "a":
+    r = numero1 + numero2
+    print(GREEN+"La respuesta es:", r)
+elif e == "b":
+    r = numero1 - numero2
+    print(GREEN+"La respuesta es:", r)
+if e == "c":
+    r = numero1 * numero2
+    print(GREEN+"La respuesta es:", r)
+elif e == "d":
+    r = numero1 / numero2
+    print(GREEN+"La respuesta es:", r)
+if e == "e":
+    r = numero1 % numero2
+    print(GREEN+"La respuesta es:", r)
+elif e == "f":
+    r = numero1 ** numero2
+    print(GREEN+"La respuesta es:", r)
+if e == "A":
+    r = numero1 + numero2
+    print(GREEN+"La respuesta es:", r)
+elif e == "B":
+    r = numero1 - numero2
+    print(GREEN+"La respuesta es:", r)
+if e == "C":
+    r = numero1 * numero2
+    print(GREEN+"La respuesta es:", r)
+elif e == "D":
+    r = numero1 / numero2
+    print(GREEN+"La respuesta es:", r)
+if e == "E":
+    r = numero1 % numero2
+    print(GREEN+"La respuesta es:", r)
+elif e == "F":
+    r = numero1 ** numero2
+    print(GREEN+"La respuesta es:", r)
+
+print("")
+
+print(BLUE+"Gracias por utilizarme hasta luego uwu.")
+
+
+
+
+
+
+
+
+
